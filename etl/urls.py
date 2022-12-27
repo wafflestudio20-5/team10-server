222168673a5809813ebd2b3bc45c403df20cacd3
LimSusu <dlatn4389@snu.ac.kr>
2022-12-27 10:44:31 +0900

    sprint1 시작을 위한 초기화, authentication 앱 생성

--- etl/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from .views import *
-
-urlpatterns = [
-    path('posts/', PostListCreateView.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
