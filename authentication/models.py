3e629d6bbb5496f4128e730e28578fdbe37870e7
LimSusu <dlatn4389@snu.ac.kr>
2022-12-27 15:38:52 +0900

    user model, urls, serializer 생성

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_professor = True
+        user.save(using=self._db)
+        return user
+
+
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, validators=[username_validator], null=True)
+    student_id = models.TextField(max_length=10, null=True)
+    is_active = models.BooleanField(default=True)
+    is_professor = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_qualified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
