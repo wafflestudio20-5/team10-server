4c65af73bf0bd067c29b92aaa8064bec1dc4646a
kk0in <kyi8871@naver.com>
2022-12-29 16:15:15 +0900

    social login

--- team10_server/urls.py
+++ team10_server/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
+    path('accounts/', include('allauth.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
