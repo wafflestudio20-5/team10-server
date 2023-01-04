6c897f1da9ed7d5ca21a139dc464592ad2856e1d
LimSusu <dlatn4389@snu.ac.kr>
2023-01-04 15:56:23 +0900

    swagger 추가

--- team10_server/settings.py
+++ team10_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'authentication.apps.AuthenticationConfig',
