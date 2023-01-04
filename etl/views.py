89c85b56b41d5c696bc0fe83d0e101597e67033a
LimSusu <dlatn4389@snu.ac.kr>
2023-01-04 17:31:44 +0900

    enroll class 기능 추가

--- etl/views.py
+++ etl/views.py
@@ -1,10 +1,13 @@
 from .serializers import *
 from rest_framework import generics, permissions
 from .models import *
+from authentication.serializers import UserSerializer
+from .permissions import *
+from rest_framework.response import Response
 
 
 class ClassListView(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
 
@@ -12,3 +15,15 @@ class ClassListView(generics.ListAPIView):
 class ClassCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAdminUser]
     serializer_class = ClassSerializer
+
+
+class EnrollClassView(generics.RetrieveAPIView):
+    permission_classes = [DoesUserMatchRequest]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get(self, *args, **kwargs):
+        class_id = int(self.request.GET['class_id'])
+        lecture = Class.objects.get(id=class_id)
+        self.request.user.classes.add(lecture)
+        return super().get(self, *args, **kwargs)
