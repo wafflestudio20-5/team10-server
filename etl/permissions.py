028f0ccbc236affd6324e819b841fee1426b2c48
LimSusu <dlatn4389@snu.ac.kr>
2023-01-06 10:08:26 +0900

    permissons.py 업데이트(주석 추가, 클래스 이름 수정)

--- etl/permissions.py
+++ etl/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 from authentication.models import User
 
 
+# 현재 요청을 날린 유저가 접근하고자 하는 유저 정보가 본인의 정보인지 확인
 class DoesUserMatchRequest(permissions.BasePermission):
     def has_permission(self, request, view):
         user_id = view.kwargs['pk']
@@ -10,6 +11,9 @@ class DoesUserMatchRequest(permissions.BasePermission):
         return False
 
 
-class IsAuthenticated(permissions.BasePermission):
+# 현재 요청을 날린 유저가 개인정보(이름, 학번) 설정이 완료되어 etl의 서비스에 접근할 수 있는지 확인
+# 프론트엔드 단에서 username==null or student_id==null 조건문을 통해 적절히 분기하겠지만, 백엔드 단에서 한 번 더 확인해주기 위함.
+class IsQualified(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.student_id is not None)
+        return bool(
+            request.user.is_authenticated and request.user.username_validator is not None and request.user.student_id is not None)
