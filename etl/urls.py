36d55a8a49b22abe98902650bf00e71e280f32db
LHJ1q <seers.louis@gmail.com>
2023-01-11 03:55:50 +0900

    Assignment

--- etl/urls.py
+++ etl/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('class/enroll/', EnrollClassView.as_view()),
     path('class/drop/', DropClassView.as_view()),
     path('class/<int:pk>/user-list/', StudentListView.as_view()),
+    path('assignments/', AssignmentListCreateView.as_view()),
+    path('assignments/<int:pk>/', AssignmentClassView.as_view()),
+    path('assignments/class/<int:pk>/', AssignmentListByLectureView.as_view()),
+    path('assignments/student/', AssignmentListByStudentView.as_view()),
+    path('assignments/grade/<int:pk>/', AssignmentGradeGetView.as_view()),
+    path('assignments/grading/<int:pk>/', AssignmentGradingView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
