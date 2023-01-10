2450d8c32211fc892d97b9668c8b0b0d604d760a
LHJ1q <seers.louis@gmail.com>
2023-01-11 03:55:50 +0900

    Assignment

--- etl/views.py
+++ etl/views.py
@@ -47,3 +47,65 @@ class StudentListView(generics.ListAPIView):
 
     def get_queryset(self):
         return User.objects.filter(classes=self.kwargs['pk'])
+
+#GET assignments/
+#모든 assignments list 반환
+class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentCreateSerializer
+    permission_classes = [IsProfessorOrReadOnly | IsAdmin]
+
+
+#GET, PUT, PATCH, DELETE assignments/<int:pk>/
+#pk번째 assignments detail 반환, 수정, 삭제
+class AssignmentClassView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentDetailSerializer
+    permission_list = {
+        'PATCH': [IsCreatorReadOnly | IsAdmin],
+        'PUT': [IsCreatorReadOnly | IsAdmin],
+        'DELETE': [IsCreatorReadOnly | IsAdmin],
+        'GET': [IsQualified]
+    }
+    def get_permissions(self):
+        if self.request.method in self.permission_list:
+            permission_classes = self.permission_list.get(self.request.method)
+            return [permission() for permission in permission_classes]
+        return super().get_permissions()
+
+
+#GET assignments/class/<int:pk>/
+#pk번째 class의 모든 assignment list 반환
+class AssignmentListByLectureView(generics.ListAPIView):
+    serializer_class = AssignmentCreateSerializer
+    permission_classes = [IsQualified]
+
+    def get_queryset(self):
+        return Assignment.objects.all().filter(lecture=self.kwargs['pk'])
+
+#GET assignments/student/
+#학생이 자신의 모든 assignment list 반환
+class AssignmentListByStudentView(generics.ListAPIView):
+    serializer_class = AssignmentCreateSerializer
+    permission_classes = [IsQualified]
+
+    def get_queryset(self):
+        return Assignment.objects.all().filter(student=self.request.user)
+
+#GET assignments/grade/<int:pk>/
+#학생이 pk번째 assignment 점수, 제출여부, 채점여부 확인
+class AssignmentGradeGetView(generics.RetrieveAPIView):
+    serializer_class = AssignmentToStudentSerializer
+    permission_classes = [IsQualified]
+
+    def get(self, request, *args, **kwargs):
+        obj = AssignmentToStudent.objects.get(assignment=self.kwargs['pk'], student=self.request.user)
+        serializer = AssignmentToStudentSerializer(obj)
+        return Response(serializer.data)
+
+#PUT(PATCH) assignments/grading/<int:pk>/
+#교수자가 pk번재 assignments 채점. 학번, 점수 입력
+class AssignmentGradingView(generics.UpdateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class=AssignmentGradingSerializer
+    permission_classes = [IsCreatorReadOnly | IsAdmin]
