cffdd197afc7220cd6fd3d427a1b46fe933c4db4
LimSusu <dlatn4389@snu.ac.kr>
2023-01-11 09:35:21 +0900

    swagger 수정

--- etl/views.py
+++ etl/views.py
@@ -5,6 +5,8 @@ from .models import *
 from .permissions import *
 from .paginations import *
 from rest_framework.response import Response
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 class ClassListView(generics.ListCreateAPIView):
@@ -17,6 +19,36 @@ class EnrollClassView(generics.CreateAPIView):
     permission_classes = [IsAdmin]
     serializer_class = EnrollDropSerializer
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            'Enroll',
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'class_id': openapi.Schema('class id', type=openapi.TYPE_INTEGER),
+            },
+            description='현재 로그인된 유저가 수강신청할 Class의 id를 입력으로 받습니다.',
+        ),
+        responses={
+            201: openapi.Schema(
+                'Enroll',
+                description='수강신청 이후 현재 로그인된 유저가 수강하는 Class list를 반환합니다.',
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'classes': openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=openapi.Schema(
+                            'Class',
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                'id': openapi.Schema('ID', read_only=True, type=openapi.TYPE_INTEGER),
+                                'name': openapi.Schema('name', type=openapi.TYPE_STRING),
+                            },
+                        ),
+                    ),
+                }
+            ),
+        },
+    )
     def post(self, request, *args, **kwargs):
         class_id = int(request.data['class_id'])
         lecture = Class.objects.get(id=class_id)
@@ -30,7 +62,36 @@ class DropClassView(generics.CreateAPIView):
     permission_classes = [IsAdmin]
     serializer_class = EnrollDropSerializer
 
-    # 코드에 대한 설명은 EnrollClassView 와 유사하므로 이를 참조하기 바람.
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            'Drop',
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'class_id': openapi.Schema('class id', type=openapi.TYPE_INTEGER),
+            },
+            description='현재 로그인된 유저가 수강신청할 Class의 id를 입력으로 받습니다.',
+        ),
+        responses={
+            201: openapi.Schema(
+                'Drop',
+                description='드랍 이후 현재 로그인된 유저가 수강하는 Class list를 반환합니다.',
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'classes': openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=openapi.Schema(
+                            'Class',
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                'id': openapi.Schema('ID', read_only=True, type=openapi.TYPE_INTEGER),
+                                'name': openapi.Schema('name', type=openapi.TYPE_STRING),
+                            },
+                        ),
+                    ),
+                }
+            ),
+        },
+    )
     def post(self, request, *args, **kwargs):
         class_id = int(request.data['class_id'])
         lecture = Class.objects.get(id=class_id)
