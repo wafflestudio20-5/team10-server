b5fdddd994eb48cf93e753e16093df4b6a3bfaf7
LimSusu <dlatn4389@snu.ac.kr>
2023-01-13 07:52:27 +0900

    김영인 수정분 반영

--- etl/paginations.py
+++ etl/paginations.py
@@ -4,3 +4,9 @@ from rest_framework.pagination import CursorPagination
 class StudentListPagination(CursorPagination):
     ordering = 'username'
     page_size = 2
+
+
+class PostListPagination(CursorPagination):
+    ordering = '-created_at'
+    page_size = 10
+
--- etl/urls.py
+++ etl/urls.py
@@ -17,6 +17,23 @@ urlpatterns = [
     path('assignments/student/', AssignmentListByStudentView.as_view()),
     path('assignments/grade/<int:pk>/', AssignmentGradeGetView.as_view()),
     path('assignments/grading/<int:pk>/', AssignmentGradingView.as_view()),
+
+    # 공지사항 게시판
+    path('announcement/', AnnouncementListView.as_view(), name='announcement_list'),
+    path('announcement/<int:pk>/', AnnouncementDetailView.as_view(), name='announcement_detail'),
+    path('announcement/<int:pk>/comment/create/', CommentCreateView.as_view(), name='announcement_comment_create'),
+    path('announcement/create/', AnnouncementCreateView.as_view(), name='announcement_create'),
+    path('announcement/modify/<int:pk>/', AnnouncementUpdateView.as_view(), name='announcement_update'),
+    path('announcement/delete/<int:pk>/', AnnouncementDeleteView.as_view(), name='announcement_delete'),
+
+    # 질의응답 게시판
+    path('question/', QuestionListView.as_view(), name='question_list'),
+    path('question/<int:pk>/', QuestionDetailView.as_view(), name='question_detail'),
+    path('question/<int:pk>/comment/create/', CommentCreateView.as_view(), name='question_comment_create'),
+    path('question/create/', QuestionCreateView.as_view(), name='question_create'),
+    path('question/modify/<int:pk>/', QuestionUpdateView.as_view(), name='question_update'),
+    path('question/delete/<int:pk>/', QuestionDeleteView.as_view(), name='question_delete'),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
