ce9ebc28905e97466886a6e9875caf326981ff9b
LimSusu <dlatn4389@snu.ac.kr>
2023-01-20 21:21:09 +0900

    github action 테스트(24): SECRET_KEY 숨김

--- team10_server/settings.py
+++ team10_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n)+x1edk-o6n%kh86fjlkz-f#rtfz0ocdl!7jzr8e^u!5+lg0='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
