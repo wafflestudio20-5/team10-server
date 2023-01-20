0122e5b98dbabe36372894f695ace484b9d9367e
LimSusu <dlatn4389@snu.ac.kr>
2023-01-20 19:04:08 +0900

    github action 테스트(22): .env 사용 X

--- team10_server/settings.py
+++ team10_server/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-n)+x1edk-o6n%kh86fjlkz-f#rtfz0ocdl!7jzr8e^u!5+lg0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['etlclonetoyproject-env.eba-a6rqj2ev.ap-northeast-2.elasticbeanstalk.com', os.environ.get('LOCAL_HOST')]
+ALLOWED_HOSTS = ['etlclonetoyproject-env.eba-a6rqj2ev.ap-northeast-2.elasticbeanstalk.com', '127.0.0.1']
 
 # Application definition
 
