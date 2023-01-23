8904b094c08b73e4ccce42c92c3542b8c8c9335b
LimSusu <dlatn4389@snu.ac.kr>
2023-01-23 09:58:28 +0900

    swagger 업데이트: LogIn 비고 추가

--- authentication/swaggers/logout.py
+++ authentication/swaggers/logout.py
@@ -1,16 +1,5 @@
 from drf_yasg import openapi
 
-logout_responses = {
-    200: openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'success': openapi.Schema(
-                type=openapi.TYPE_BOOLEAN,
-                default=True
-            )
-        }
-    )
-}
 
 logout_operation_description = '기능\n' \
                                '- 로그아웃\n' \
@@ -20,4 +9,19 @@ logout_operation_description = '기능\n' \
                                '\nrequest body\n' \
                                '- 없음\n' \
                                '\nresponses\n' \
-                               '- 200: 로그아웃 성공'
+                               '- 200: 로그아웃 성공\n' \
+                               '\n비고\n' \
+                               '- jwt 토큰으로 변경 이후 실제 로그아웃이 잘 되는지 잘 모르겠습니다.\n' \
+                               'insomnia로 확인했을 때는 되지 않았는데, 프론트엔드 단에서 확인 부탁드립니다.'
+
+logout_responses = {
+    200: openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'success': openapi.Schema(
+                type=openapi.TYPE_BOOLEAN,
+                default=True,
+            )
+        }
+    )
+}
