174f0543001da105272876c7a593ee155269e3dd
LimSusu <dlatn4389@snu.ac.kr>
2023-02-01 16:01:16 +0900

    swagger 업데이트: change-password

--- authentication/swaggers/change_password.py
+++ authentication/swaggers/change_password.py
@@ -15,12 +15,14 @@ change_password_request = openapi.Schema(
     type=openapi.TYPE_OBJECT,
     required=['new_password'],
     properties={
+        'password': openapi.Schema(
+            type=openapi.TYPE_STRING,
+        ),
         'new_password': openapi.Schema(
             type=openapi.TYPE_STRING,
             min_length=8,
         )
     },
-    description='새롭게 설정할 비밀번호를 request body에 담습니다.'
 )
 
 change_password_responses = {
@@ -43,9 +45,10 @@ change_password_responses = {
                 type=openapi.TYPE_STRING,
             )
         },
-        description='new_password가 8자 미만이거나, 비밀번호가 이전 비밀번호와 동일하다면, 이 response가 반환됩니다.'
-                    '\n1. 8자 미만일 때, "too short password. password length should be >=8."'
-                    '\n2. 이전 비밀번호와 동일할 때, "same with previous password."'
-                    '\n3. 그 외 기타 사유.'
+        description='password가 틀렸거나, new_password가 8자 미만이거나, 비밀번호가 이전 비밀번호와 동일하다면, 이 response가 반환됩니다.'
+                    '\n1. password가 틀렸을 때, "wrong password."'
+                    '\n2. new_password가 8자 미만일 때, "too short password. password length should be >=8."'
+                    '\n3. new_password가 이전 비밀번호와 동일할 때, "same with previous password."'
+                    '\n4. 그 외 기타 사유.'
     ),
 }
